df4c19f8aea5368ea4644dabdd2c989cc36b42b1
eunice <atandainioluwa371@gmail.com>
2024-11-15 06:38:34 -0800

    second push

--- components/page-illustration.tsx
+++ components/page-illustration.tsx
@@ -1,144 +1,58 @@
 // components/PageIllustration.js
-// const PageIllustration = () => (
-//     <svg xmlns="http://www.w3.org/2000/svg" version="1.1" xlinkHref="http://www.w3.org/1999/xlink" href="http://svgjs.dev/svgjs" width="1440" height="560" preserveAspectRatio="none" viewBox="0 0 1440 560">
-//     <g mask="url(&quot;#SvgjsMask1000&quot;)" fill="none">
-//         <use href="#SvgjsSymbol1007" x="0" y="0"></use>
-//         <use href="#SvgjsSymbol1007" x="720" y="0"></use>
-//     </g>
-//     <defs>
-//         <mask id="SvgjsMask1000">
-//             <rect width="1440" height="560" fill="#ffffff"></rect>
-//         </mask>
-//         <path d="M-1 0 a1 1 0 1 0 2 0 a1 1 0 1 0 -2 0z" id="SvgjsPath1006"></path>
-//         <path d="M-3 0 a3 3 0 1 0 6 0 a3 3 0 1 0 -6 0z" id="SvgjsPath1001"></path>
-//         <path d="M-5 0 a5 5 0 1 0 10 0 a5 5 0 1 0 -10 0z" id="SvgjsPath1002"></path>
-//         <path d="M2 -2 L-2 2z" id="SvgjsPath1004"></path>
-//         <path d="M6 -6 L-6 6z" id="SvgjsPath1003"></path>
-//         <path d="M30 -30 L-30 30z" id="SvgjsPath1005"></path>
-//     </defs>
-//     <symbol id="SvgjsSymbol1007">
-//         <use href="#SvgjsPath1001" x="30" y="30" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="30" y="90" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="30" y="150" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="30" y="210" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="30" y="270" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1005" x="30" y="330" stroke="rgba(204, 29, 65, 1)" stroke-width="3"></use>
-//         <use href="#SvgjsPath1002" x="30" y="390" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="30" y="450" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="30" y="510" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1001" x="30" y="570" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1006" x="90" y="30" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="90" y="90" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="90" y="150" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="90" y="210" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1001" x="90" y="270" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="90" y="330" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1006" x="90" y="390" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1001" x="90" y="450" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="90" y="510" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1006" x="90" y="570" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="150" y="30" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="150" y="90" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="150" y="150" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="150" y="210" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="150" y="270" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1005" x="150" y="330" stroke="rgba(204, 29, 65, 1)" stroke-width="3"></use>
-//         <use href="#SvgjsPath1003" x="150" y="390" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="150" y="450" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="150" y="510" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="150" y="570" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1006" x="210" y="30" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="210" y="90" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="210" y="150" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="210" y="210" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1001" x="210" y="270" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="210" y="330" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="210" y="390" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="210" y="450" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="210" y="510" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="210" y="570" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="270" y="30" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="270" y="90" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="270" y="150" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="270" y="210" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1001" x="270" y="270" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="270" y="330" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="270" y="390" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="270" y="450" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="270" y="510" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1001" x="270" y="570" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="330" y="30" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="330" y="90" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1001" x="330" y="150" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="330" y="210" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="330" y="270" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="330" y="330" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="330" y="390" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1001" x="330" y="450" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="330" y="510" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="330" y="570" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="390" y="30" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1006" x="390" y="90" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="390" y="150" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="390" y="210" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="390" y="270" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="390" y="330" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="390" y="390" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1001" x="390" y="450" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1001" x="390" y="510" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="390" y="570" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1001" x="450" y="30" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="450" y="90" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1005" x="450" y="150" stroke="rgba(204, 29, 65, 1)" stroke-width="3"></use>
-//         <use href="#SvgjsPath1003" x="450" y="210" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="450" y="270" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="450" y="330" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="450" y="390" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="450" y="450" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="450" y="510" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="450" y="570" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1006" x="510" y="30" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1001" x="510" y="90" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="510" y="150" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="510" y="210" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="510" y="270" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="510" y="330" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="510" y="390" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="510" y="450" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1006" x="510" y="510" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="510" y="570" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="570" y="30" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="570" y="90" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1005" x="570" y="150" stroke="rgba(204, 29, 65, 1)" stroke-width="3"></use>
-//         <use href="#SvgjsPath1004" x="570" y="210" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="570" y="270" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="570" y="330" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1005" x="570" y="390" stroke="rgba(204, 29, 65, 1)" stroke-width="3"></use>
-//         <use href="#SvgjsPath1004" x="570" y="450" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="570" y="510" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1006" x="570" y="570" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="630" y="30" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1006" x="630" y="90" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="630" y="150" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1006" x="630" y="210" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1005" x="630" y="270" stroke="rgba(204, 29, 65, 1)" stroke-width="3"></use>
-//         <use href="#SvgjsPath1003" x="630" y="330" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="630" y="390" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="630" y="450" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1003" x="630" y="510" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="630" y="570" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="690" y="30" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1001" x="690" y="90" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1005" x="690" y="150" stroke="rgba(204, 29, 65, 1)" stroke-width="3"></use>
-//         <use href="#SvgjsPath1004" x="690" y="210" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="690" y="270" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="690" y="330" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="690" y="390" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="690" y="450" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1002" x="690" y="510" stroke="rgba(204, 29, 65, 1)"></use>
-//         <use href="#SvgjsPath1004" x="690" y="570" stroke="rgba(204, 29, 65, 1)"></use>
-//     </symbol>
-// </svg>
-
+const PageIllustration = () => (
+    <div className="svg-background">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100" preserveAspectRatio="none" width="100%" height="100%">
+        <defs>
+          <linearGradient id="grad1" x1="0%" y1="0%" x2="100%" y2="100%">
+            <stop offset="0%" style={{ stopColor: '#F28D9C', stopOpacity: 1 }} /> {/* Light Crimson */}
+            <stop offset="50%" style={{ stopColor: '#D9535A', stopOpacity: 1 }} /> {/* Dark Crimson */}
+            <stop offset="100%" style={{ stopColor: '#F24C61', stopOpacity: 1 }} /> {/* Darker Crimson */}
+          </linearGradient>
+        </defs>
   
-//   export default PageIllustration;
+        {/* Animated Circles */}
+        <circle cx="20" cy="20" r="4" fill="url(#grad1)">
+          <animate attributeName="cx" from="0" to="50" dur="3s" repeatCount="indefinite" keyTimes="0;0.5;1" values="0;50;100" />
+          <animate attributeName="cy" from="0" to="50" dur="3s" repeatCount="indefinite" keyTimes="0;0.5;1" values="0;50;100" />
+          <animateTransform attributeName="transform" type="rotate" from="0 20 20" to="360 20 20" dur="6s" repeatCount="indefinite"/>
+        </circle>
+  
+        <circle cx="70" cy="30" r="3" fill="url(#grad1)">
+          <animate attributeName="cx" from="70" to="30" dur="5s" repeatCount="indefinite" keyTimes="0;0.5;1" values="70;30;70" />
+          <animate attributeName="cy" from="30" to="70" dur="5s" repeatCount="indefinite" keyTimes="0;0.5;1" values="30;70;30" />
+          <animateTransform attributeName="transform" type="scale" from="1" to="1.5" dur="5s" repeatCount="indefinite"/>
+        </circle>
+  
+        <circle cx="50" cy="60" r="5" fill="url(#grad1)">
+          <animate attributeName="cx" from="50" to="100" dur="6s" repeatCount="indefinite" keyTimes="0;0.5;1" values="50;100;50" />
+          <animate attributeName="cy" from="60" to="10" dur="6s" repeatCount="indefinite" keyTimes="0;0.5;1" values="60;10;60" />
+          <animateTransform attributeName="transform" type="rotate" from="0 50 60" to="360 50 60" dur="7s" repeatCount="indefinite"/>
+        </circle>
+  
+        <circle cx="40" cy="40" r="2" fill="url(#grad1)">
+          <animate attributeName="cx" from="40" to="80" dur="4s" repeatCount="indefinite" keyTimes="0;0.5;1" values="40;80;40" />
+          <animate attributeName="cy" from="40" to="80" dur="4s" repeatCount="indefinite" keyTimes="0;0.5;1" values="40;80;40" />
+          <animateTransform attributeName="transform" type="scale" from="0.5" to="2" dur="4s" repeatCount="indefinite"/>
+        </circle>
+  
+        <circle cx="80" cy="70" r="3" fill="url(#grad1)">
+          <animate attributeName="cx" from="80" to="40" dur="6s" repeatCount="indefinite" keyTimes="0;0.5;1" values="80;40;80" />
+          <animate attributeName="cy" from="70" to="20" dur="6s" repeatCount="indefinite" keyTimes="0;0.5;1" values="70;20;70" />
+          <animateTransform attributeName="transform" type="rotate" from="0 80 70" to="360 80 70" dur="5s" repeatCount="indefinite"/>
+        </circle>
+      </svg>
+  
+      <style jsx>{`
+        .svg-background {
+          position: absolute;
+          top: 0;
+          left: 0;
+          width: 100%;
+          height: 100%;
+          z-index: -1; /* Keep the background behind your content */
+        }
+      `}</style>
+    </div>
+  );
+  
+  export default PageIllustration;  
